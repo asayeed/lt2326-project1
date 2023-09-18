76aa323112dd410fa7af42b9f5364dba3a1daf27
Asad Sayeed <asayeed@mbl.ca>
2023-09-18 16:53:56 +0200

    Added dataset class

--- src/covers.py
+++ src/covers.py
@@ -2,6 +2,9 @@ import os
 import sys
 from requests import get
 import pandas as pd
+from torchvision.io import read_image
+from torch.utils.data import Dataset
+from torchvision.transforms import Resize
 
 class CoverNotFound(Exception):
     pass
@@ -22,4 +25,26 @@ def get_cover(isbn, size="L", filename=None):
 # The above is kind of useless since the kaggle challenge already contained the covers but... :D
 
 def get_covers_table(tablefilename):
-    return pd.read_table
+    return pd.read_csv(tablefilename, index_col=None)
+
+class CoversDataset(Dataset):
+    def __init__(self, tablefilename, imgroot, size):
+        self.covertable = get_covers_table(tablefilename)
+        self.imgroot = imgroot
+        self.resize = Resize(size)
+
+        categorylist = list(set(list(self.covertable['category'])))
+        categoryvals = list(range(len(categorylist)))
+        self.cat2label = dict(zip(categorylist, categoryvals))
+
+        self.covertable['label'] = [self.cat2label[x] for x in self.covertable['category']]
+
+    def __len__(self):
+        return len(self.covertable)
+
+    def __getitem__(self, idx): 
+        row = self.covertable.iloc[idx]
+        imagepath = os.path.join(self.imgroot, row['img_paths'])
+        label = row['label']
+        image = self.resize(read_image(imagepath))
+        return image, label
