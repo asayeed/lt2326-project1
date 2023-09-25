537e6d69793ed4bf886034d0d16b6266ce0c6e93
Asad Sayeed <asayeed@mbl.ca>
2023-09-25 15:23:22 +0200

    built model and training loop actually run, still bugs

--- src/covers.py
+++ src/covers.py
@@ -3,11 +3,13 @@ import sys
 from requests import get
 import pandas as pd
 from torchvision.io import read_image
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, random_split
 from torchvision.transforms import Resize
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch
+from tqdm import tqdm
+from torch import optim
 
 class CoverNotFound(Exception):
     pass
@@ -49,23 +51,50 @@ class CoversDataset(Dataset):
         row = self.covertable.iloc[idx]
         imagepath = os.path.join(self.imgroot, row['img_paths'])
         label = row['label']
-        image = self.resize(read_image(imagepath))
+        image = self.resize(read_image(imagepath)).float()
         return image, label
 
 # Now we build our model.
 class CoversGenreModel(nn.Module):
-    def __init__(self, outputsize):
+    def __init__(self, outputsize, xdim, ydim):
         super().__init__()
         self.outputsize = outputsize
 
-        self.conv2d = torch.nn.Conv2d(3, 1, (3,3))
+        self.conv2d = torch.nn.Conv2d(3, 1, (3,3), padding=1)
+        self.linear = torch.nn.Linear(xdim*ydim, outputsize)
     
     def forward(self, batch):
         output = self.conv2d(batch).squeeze(1)
-        batchsize = output.size[0]
+        batchsize = output.size()[0]
         dim1 = output.size()[1]
         dim2 = output.size()[2]
         output = output.view((batchsize, dim1*dim2))
+        output = self.linear(output)
         return torch.log_softmax(output, 1)
 
+def create_env(tablefilename, imgroot, imgsize, testsize=0.3):
+    dataset = CoversDataset(tablefilename, imgroot, imgsize)
+    train_set, test_set = random_split(dataset, (1.0-testsize, testsize))    
+    model = CoversGenreModel(33, imgsize[0], imgsize[1])
+
+    return train_set, test_set, model
+
+def train(train_set, model, epochs=25, batch_size=50):
+    trainloader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.parameters())
+    
+    for epoch in range(epochs):
+        total_loss = 0
+        for i, batch in enumerate(tqdm(trainloader)):
+            optimizer.zero_grad()
+            X, y = batch
+            output = model(X)
+            loss = criterion(output, y)
+            loss.backward()
+            total_loss += int(loss)
+            optimizer.step()
+
+        print("At epoch {}, we get loss {}.".format(epoch, total_loss))
 
